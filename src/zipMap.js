92e8f708fc99b787335ba07a51ddcc7ab413af04
Haoran Yu <hy47@cs.washington.edu>
2020-03-02 01:29:49 -0800

    convert to d3 version

--- src/zipMap.js
+++ src/zipMap.js
@@ -1,21 +1,38 @@
 class ZipMap {
     constructor() {
+        this.data = {};
+        const zipData = require('./zipcode.json');
+        for (let i = 0; i < zipData["features"].length; i++) {
+            this.data[zipData["features"][i]["properties"]["ZCTA5CE10"]] = zipData["features"][i]["geometry"]["coordinates"]
+        }
     }
     drawMap() {
-        const zipData = require('./zipcode.json');
-        let dat = {};
-        for (let i = 0; i < zipData["features"].length; i++) {
-            dat[zipData["features"][i]["properties"]["ZCTA5CE10"]] = zipData["features"][i]["geometry"]["coordinates"]
-        }
         const d3 = require('d3');
-        let svg = d3.select("svg").append("g");
-        for (let zipCode in dat) {
-            let coor = dat[zipCode][0];
-            for (let i = 0; i < coor.length - 1; i++) {
-                svg.append("line").style("stroke", "green").style("stroke-width", 1).attr("x1", (coor[i][0]+123)*1000-500).attr("y1", (48-coor[i][1])*1000)
-                    .attr("x2", (coor[i+1][0]+123)*1000-500).attr("y2", (48-coor[i+1][1])*1000);
-            }
-        }
+        const zipData = require('./zipcode.json');
+
+        const w = 1000;
+        const h = 1300;
+
+        let projection = d3.geoAlbers()
+            .translate([w/6, h/2])
+            .scale([100000])
+            .center([0,47.5])
+            .rotate([122.4,0]);
+
+        let path = d3.geoPath()
+            .projection(projection);
+
+        let svg = d3.select("#map")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+        let map = svg.selectAll("path")
+            .data(zipData.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .attr("class", "district");
     }
 }
 
