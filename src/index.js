c2364bf8557233b1620b6cc15b063f2b717ce55d
douy <douy@cs.washington.edu>
2020-03-02 13:07:55 -0800

    finish fullpage template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,80 @@
+console.log(module);
+var fullpage = require('./fullpage');
+
+var myFullpage = new fullpage('#fullpage', {
+	//Navigation
+    menu: '#menu',
+    lockAnchors: false,
+    anchors:['firstPage', 'secondPage',  'thirdPage', 'fourthPage', 'fifthPage'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['first', 'second', 'third', 'fourth', 'fifth'],
+    // showActiveTooltip: false,
+    // slidesNavigation: true,
+    // slidesNavPosition: 'bottom',
+
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 700,
+    autoScrolling: true,
+    fitToSection: false,
+    fitToSectionDelay: 100,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: true,
+    continuousVertical: false,
+    continuousHorizontal: false,
+    scrollHorizontally: false,
+    interlockedSlides: false,
+    dragAndMove: false,
+    offsetSections: false,
+    resetSliders: false,
+    fadingEffect: false,
+    normalScrollElements: '#element1, .element2',
+    scrollOverflow: false,
+    scrollOverflowReset: false,
+    scrollOverflowOptions: null,
+    touchSensitivity: 15,
+    normalScrollElementTouchThreshold: 5,
+    bigSectionsDestination: null,
+
+    //Accessibility
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+
+    //Design
+    controlArrows: true,
+    verticalCentered: false,
+    //sectionsColor : ['#fcfcfc', '#fcfcfc'],
+    //paddingTop: '0em',
+    paddingBottom: '10px',
+    fixedElements: '#header, .footer',
+    responsiveWidth: 0,
+    responsiveHeight: 0,
+    responsiveSlides: false,
+    parallax: false,
+    parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+    //Custom selectors
+    sectionSelector: '.section',
+    slideSelector: '.slide',
+
+    lazyLoading: true,
+
+    //events
+    onLeave: function(index, nextIndex, direction){},
+    afterLoad: function(origin, destination, direction){},
+    afterRender: function(){},
+    afterResize: function(){},
+    afterResponsive: function(isResponsive){},
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+    onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+});
+
 const zipMap = require('./zipMap');
 const zipMapInstance = new zipMap();
 zipMapInstance.drawMap();
