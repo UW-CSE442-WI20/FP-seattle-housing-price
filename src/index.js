7e986690a18bd39e7adef54c6e4d41a55cd3b611
Haoran Yu <hy47@cs.washington.edu>
2020-03-02 02:56:54 -0800

    mostly finished without data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
 const zipMap = require('./zipMap');
 const zipMapInstance = new zipMap();
 zipMapInstance.drawMap();
-const zipData = zipMapInstance.data;
-let btn = document.getElementById("zipSubmit");
-btn.onclick = function () {
-    const zip = document.getElementById("zipChosen").value;
-    if (zip in zipData) {
-        alert(zip);
-    } else {
-        alert("Please enter valid zip code");
-    }
-};
--- src/zipMap.js
+++ src/zipMap.js
@@ -1,38 +1,64 @@
 class ZipMap {
-    constructor() {
-        this.data = {};
-        const zipData = require('./zipcode.json');
-        for (let i = 0; i < zipData["features"].length; i++) {
-            this.data[zipData["features"][i]["properties"]["ZCTA5CE10"]] = zipData["features"][i]["geometry"]["coordinates"]
-        }
-    }
+    constructor() {}
+
     drawMap() {
         const d3 = require('d3');
         const zipData = require('./zipcode.json');
-
         const w = 1000;
         const h = 1300;
+        let center = {};
 
-        let projection = d3.geoAlbers()
-            .translate([w/6, h/2])
-            .scale([100000])
-            .center([0,47.5])
-            .rotate([122.4,0]);
+        let projection = d3.geoAlbers().translate([w/6, h/2]).scale([100000]).center([0,47.5]).rotate([122.4,0]);
+        let path = d3.geoPath().projection(projection);
+        let svg = d3.select("#map").append("svg").attr("width", w).attr("height", h);
+        let map = svg.selectAll("path").data(zipData.features).enter().append("path")
+            .attr("d", path).attr("class", "district")
+            .attr("id", function(d) {
+                let zip = d.properties.ZCTA5CE10;
+                center[zip] = path.centroid(d);
+                return zip;
+            })
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut)
+            .on("click", handleMouseClick);
 
-        let path = d3.geoPath()
-            .projection(projection);
+        function handleMouseOver() {
+            d3.select(this).style("opacity", .7);
+        }
 
-        let svg = d3.select("#map")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
+        function handleMouseOut() {
+            d3.select(this).style("opacity", 1);
+        }
 
-        let map = svg.selectAll("path")
-            .data(zipData.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .attr("class", "district");
+        let choose = [];
+
+        function handleMouseClick() {
+            if (choose.length < 2) {
+                d3.select(this).style("fill", "#66CCFF");
+                choose.push(this);
+            } else {
+                alert("Please reset the chosen area")
+            }
+        }
+
+        let submit = document.getElementById("zipSubmit");
+        submit.onclick = function() {
+            let zip = document.getElementById("zipChosen").value;
+            if (zip in center) {
+                svg.append("circle").attr("cx", center[zip][0]).attr("cy", center[zip][1]).attr("r", "100px").style("fill", "transparent").style("stroke", "black")
+            } else {
+                alert("Please enter valid zip code!")
+            }
+
+            console.log(center["98105"])
+        };
+
+        let reset = document.getElementById("chooseReset");
+        reset.onclick = function () {
+            d3.select(choose.pop()).style("fill", "D3D3D3");
+            d3.select(choose.pop()).style("fill", "D3D3D3");
+        };
+
     }
 }
 
