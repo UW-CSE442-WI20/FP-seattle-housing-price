a10dd9e3d438f0bf7502930996677068862431d5
bowenxu <xubowenhaoren@gmail.com>
2020-03-03 00:35:11 -0800

    fix color scheme

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var myFullpage = new fullpage('#fullpage', {
 	//Navigation
     menu: '#menu',
     lockAnchors: false,
-    sectionsColor: ['black', 'black', 'black', 'black', 'black'],
+    sectionsColor: ['#29303F', '#29303F', '#29303F', '#29303F', '#29303F'],
     anchors:['firstPage', 'secondPage',  'thirdPage', 'fourthPage', 'fifthPage'],
     navigation: true,
     navigationPosition: 'right',
