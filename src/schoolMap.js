5ba33b73200b14c2275ae47604a4e7c6c8275a79
bowenxu <xubowenhaoren@gmail.com>
2020-03-09 21:29:15 -0700

    update housing price data

--- src/schoolMap.js
+++ src/schoolMap.js
@@ -5,6 +5,16 @@ class schoolMap {
     drawMap(d3, zipData, schoolData) {
         let h = document.documentElement.scrollHeight - 10;
         let w = document.documentElement.scrollWidth / 2;
+        var loColorHiColor = ["#e3ecfc", "#173463"];
+        var gradient = ["#E5F6FF", "#ACCEE1", "#73A7C4", "#3A7FA7", "#02588A"];
+        var strokeColor = "white";
+        var strokeHighlightColor = "#F6A600";
+        var strokeWidth = 2;
+        var strokeHighlightWidth = 3;
+        var darkgrey = "#4c4c4c";
+        var color = d3.scaleQuantize()
+            .domain([0, 30])
+            .range(gradient);
         var toolTipG;
         var toolTipWidth = 150, toolTipHeight = 50;
         var toolTipText = "No data";
@@ -24,7 +34,10 @@ class schoolMap {
         // let svg1 = d3.select("#compare").append("svg").attr("width", w).attr("height", h);
         //
         let map = svg.selectAll("path").data(zipData.features).enter().append("path")
-            .attr("d", path).style("fill", "#D3D3D3").style("stroke", "white")
+            .attr("d", path).style("fill", function(d) {
+                return mapFillFunct(d);
+            })
+            .style("stroke", "white")
             .attr("id", function(d) {
                 let zip = d.properties.ZCTA5CE10;
                 center[zip] = path.centroid(d);
@@ -34,6 +47,67 @@ class schoolMap {
             .on("mouseout", handleMouseOut)
             .on("mousemove", handleMouseMove);
             // # .on("click", handleMouseClick);
+        var colorKeyWidth = 20, colorKeyHeight = 325, blockHeight = 40;
+        var colorKeySVG = svg.append("g")
+            .attr("transform", "translate(10, " + (h - colorKeyHeight - 10) + ")");
+        for (var i = 0; i < gradient.length; i++) {
+            colorKeySVG.append("rect")
+                .datum([30 - (i + 1) * 6])
+                .attr("x", 0)
+                .attr("y", i * blockHeight)
+                .attr("width", colorKeyWidth)
+                .attr("height", blockHeight)
+                .attr("fill", function(d) {
+                    // console.log(d);
+                    return color(d);
+                });
+        }
+        var colorScale = d3.scaleLinear()
+            .domain([0, 30])
+            .range([200, 0])
+            .nice();
+        colorKeySVG.append("g")
+            .call(d3.axisRight(colorScale)
+                // .tickFormat(d3.format("$,"))
+                .tickSize(0)
+                .tickValues(
+                    colorScale.ticks(5)
+                        .concat(colorScale.domain())))
+            .attr("transform", "translate(" + colorKeyWidth + ", 0)")
+            .selectAll("text")
+            .attr("class", "unselectable")
+            .attr("font-family", "Open Sans")
+            .attr('font-size', "12px")
+            .attr("fill", darkgrey)
+            .attr("dx", "2px");
+
+        colorKeySVG.selectAll("path")
+            .remove();
+
+        colorKeySVG.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("transform", "translate(0, -40)")
+            .attr("dy", "1em")
+            .attr("fill", darkgrey)
+            .attr("class", "unselectable")
+            .text("Number of Schools");
+
+        colorKeySVG.append("rect")
+            .attr("x", 0)
+            .attr("y", 225)
+            .attr("width", colorKeyWidth)
+            .attr("height", colorKeyWidth)
+            .attr("fill", "black");
+
+        colorKeySVG.append("text")
+            .attr("x", 30)
+            .attr("y", 235)
+            .attr("class", "unselectable")
+            .attr("dominant-baseline", "central")
+            .attr("font-family", "Open Sans")
+            .attr("font-size", "12px")
+            .text("no data");
+
 
 
         function handleMouseOver() {
@@ -73,8 +147,6 @@ class schoolMap {
                 .attr("dy", "2.4em")
                 .attr("dx", "6")
                 .text(toolTipText);
-
-
         }
 
         function handleMouseOut() {
@@ -94,108 +166,17 @@ class schoolMap {
             toolTipG.attr("transform", "translate(" + xPos +"," + yPos + ")");
         }
 
+        function mapFillFunct(d) {
+            var zipObject = data["school"][+d.properties.ZCTA5CE10];
+            if (zipObject) {
+                return color(zipObject);
+            } else {
+                return "black";
+            }
+        }
+
         let choose = [];
         let score = [];
-
-        // function handleMouseClick() {
-        //     if (choose.length < 2) {
-        //         let name = ["school"];
-        //         choose.push(this);
-        //         let yPo = 100;
-        //         if (choose.length === 1) {
-        //             d3.select(this).style("fill", "#00FFCC");
-        //             svg1.append("rect").attr("x", 300).attr("y", 95).attr("height", 205).attr("width", 100).attr("id", "rect").style("fill", "F7F7F7");
-        //             for (let i of Object.entries(data)) {
-        //                 let a = i[1][this.id];
-        //                 if (typeof a == "undefined") {
-        //                     a = 0;
-        //                 }
-        //                 let b = 300 * a / i[1]["max"];
-        //                 svg1.append("rect").attr("x", 300).attr("y", yPo).attr("height", 20).transition().duration(600)
-        //                     .attr("x", 300-b).attr("y", yPo).attr("height", 20).attr("width", b).attr("id", "rect").style("fill", "#00FFCC");
-        //                 score.push({xPo1: 300-b, yPo: yPo+15, value1: a});
-        //                 yPo += 25;
-        //             }
-        //             yPo = 115;
-        //             for (let i = 0; i < 8; i++) {
-        //                 svg1.append("text").attr("x", 350-name[i].length*4).attr("y", yPo).text(name[i]).attr("id", "name");
-        //                 yPo += 25
-        //             }
-        //             svg1.append("text").attr("x", 200).attr("y", 90).text(this.id).attr("id", "rect");
-        //         } else {
-        //             d3.select(this).style("fill", "#66CCFF");
-        //             let p = 0;
-        //             for (let i of Object.entries(data)) {
-        //                 let a = i[1][this.id];
-        //                 if (typeof a == "undefined") {
-        //                     a = 0;
-        //                 }
-        //                 let b = 300 * a / i[1]["max"];
-        //                 svg1.append("rect").attr("x", 400).attr("y", yPo).attr("height", 20)
-        //                     .transition().duration(600).attr("width", b).attr("id", "rect").style("fill", "#66CCFF");
-        //                 yPo += 25;
-        //                 score[p]["xPo2"] = 400+b;
-        //                 score[p]["value2"] = a;
-        //                 p++;
-        //             }
-        //             svg1.append("text").attr("x", 450).attr("y", 90).text(this.id).attr("id", "rect");
-        //             d3.selectAll("#name").style("cursor", "pointer").on("click", function(d, i){
-        //                 d3.selectAll("#score").remove();
-        //                 svg1.append("text").attr("x", score[i].xPo1-30).attr("y", score[i].yPo).text(score[i].value1).attr("id", "score");
-        //                 svg1.append("text").attr("x", score[i].xPo2+5).attr("y", score[i].yPo).text(score[i].value2).attr("id", "score");
-        //                 svg1.append("line").attr("x1", score[i].xPo1).attr("y1", 100).attr("x2", score[i].xPo1).attr("y2", 345)
-        //                     .style("stroke-dasharray", "2 2").style("stroke", "black").attr("id", "score");
-        //                 svg1.append("line").attr("x1", score[i].xPo2).attr("y1", 100).attr("x2", score[i].xPo2).attr("y2", 345)
-        //                     .style("stroke-dasharray", "2 2").style("stroke", "black").attr("id", "score");
-        //                 svg1.append("text").attr("x", 345).attr("y", 360).text("0").attr("id", "score");
-        //                 svg1.append("text").attr("x", score[i].xPo1-20).attr("y", 360).text(score[i].value1).attr("id", "score");
-        //                 svg1.append("text").attr("x", score[i].xPo2-20).attr("y", 360).text(score[i].value2).attr("id", "score");
-        //                 let x = 15;
-        //                 for (let i = 0; i < 50; i++) {
-        //                     svg1.append("line").attr("x1", x).attr("y1", 330).attr("x2", x).attr("y2", 345)
-        //                         .style("stroke", "black").attr("id", "score");
-        //                     x += 15;
-        //                 }
-        //                 svg1.append("rect").attr("x", score[i].xPo1).attr("y", score[i].yPo-18).attr("height", 3)
-        //                     .attr("width", 300-score[i].xPo1).attr("id", "score").style("fill", "#00FFCC");
-        //                 svg1.append("rect").attr("x", score[i].xPo1).attr("y", score[i].yPo+5).attr("height", 3)
-        //                     .attr("width", 300-score[i].xPo1).attr("id", "score").style("fill", "#00FFCC");
-        //                 svg1.append("rect").attr("x", 400).attr("y", score[i].yPo-18).attr("height", 3)
-        //                     .attr("width", score[i].xPo2-400).attr("id", "score").style("fill", "#66CCFF");
-        //                 svg1.append("rect").attr("x", 400).attr("y", score[i].yPo+5).attr("height", 3)
-        //                     .attr("width", score[i].xPo2-400).attr("id", "score").style("fill", "#66CCFF");
-        //             });
-        //         }
-        //     } else {
-        //         alert("Please reset the chosen area")
-        //     }
-        // }
-        //
-        // let submit = document.getElementById("zipSubmit");
-        // submit.onclick = function() {
-        //     svg.selectAll("#circle").remove();
-        //     let zip = document.getElementById("zipChosen").value;
-        //     let dis = document.getElementById("radiusSelect").value;
-        //     if (zip in center) {
-        //         let arc = d3.arc().innerRadius(dis*35).outerRadius(dis*35);
-        //         svg.append("path").attr("fill", "none").attr("stroke", "black").attr("d", arc({startAngle:0, endAngle: 2*Math.PI}))
-        //             .attr("transform","translate("+center[zip][0]+","+center[zip][1]+")").attr("id", "circle");
-        //         svg.append("circle").attr("cx", center[zip][0]).attr("cy", center[zip][1]).attr("r", "5px").attr("id", "circle");
-        //     } else {
-        //         alert("Please enter valid zip code!")
-        //     }
-        // };
-        //
-        // let reset = document.getElementById("chooseReset");
-        // reset.onclick = function () {
-        //     score = [];
-        //     d3.select(choose.pop()).style("fill", "D3D3D3");
-        //     d3.select(choose.pop()).style("fill", "D3D3D3");
-        //     d3.selectAll("#rect").remove();
-        //     d3.selectAll("#name").remove();
-        //     d3.selectAll("#score").remove();
-        // };
-
 
 
     }
